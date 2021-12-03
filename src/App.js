3147cac3fd5c41b32d561f31b28df2d1e1c73040
Melanie Rodas <89095047+melanierolo@users.noreply.github.com>
2021-12-03 18:22:38 -0500

    add files

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React from "react";
+// TODO: import useFormik from formik library
+import { useFormik} from 'formik'
+
+function App() {
+  // TODO: add a const called formik assigned to useFormik()
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      email: '',
+      password: ''      
+    },
+    onSubmit: values =>{
+      console.log('form:',values);
+      let arrayValues = Object.keys(values);
+      let lengthValues =arrayValues.length;
+      if (lengthValues === 3) alert(JSON.stringify(`Login Successful ${values.name}!!!`));
+    },
+    validate: values =>{
+      let errors = {};
+      if(!values.name) errors.name = 'Required';
+      if (!values.email) {
+        errors.email = 'Required';
+      } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+        errors.email = 'Invalid email address';
+      }
+      if(!values.password) errors.password = 'Required';
+      console.log(errors);
+      return errors;
+    }
+  });
+  return (
+    <div>
+      <h1>Welcome</h1>
+      <div>
+      <form onSubmit={formik.handleSubmit}>
+        <div>Name:</div>
+        <input type="text" name="name" onChange={formik.handleChange} value={formik.values.name}/>
+        {formik.errors.name ? <div style={{color:'red'}}>{formik.errors.name}</div> : null}
+        <div>Email:</div>
+        <input type="text" name="email" id="emailField" onChange={formik.handleChange} value={formik.values.email}/>
+        {formik.errors.email ? <div id="emailError" style={{color:'red'}}>{formik.errors.email}</div> : null}        
+        <div>Password:</div>
+        <input type="text" name="password" id="pswField" onChange={formik.handleChange} value={formik.values.password}/><br/>
+        {formik.errors.password ? <div id="pswError" style={{color:'red'}}>{formik.errors.password}</div> : null}                
+        <button type="submit" id="submitBtn">Login</button>
+      </form>      
+    </div>
+    </div>
+  );
+}
+
+export default App;
