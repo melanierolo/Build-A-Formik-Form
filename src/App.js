fb1ed6935d451912540a126e4f03cb9272b7d8ee
Melanie Rodas <mrodasl@uni.pe>
2021-12-03 23:57:28 -0500

    delete name

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ function App() {
   // TODO: add a const called formik assigned to useFormik()
   const formik = useFormik({
     initialValues: {
-      name: '',
       email: '',
       password: ''      
     },
@@ -15,11 +14,10 @@ function App() {
       console.log('form:',values);
       let arrayValues = Object.keys(values);
       let lengthValues =arrayValues.length;
-      if (lengthValues === 3) alert(JSON.stringify(`Login Successful ${values.name}!!!`));
+      if (lengthValues === 2) alert(JSON.stringify(`Login Successful ${values.name}!!!`));
     },
     validate: values =>{
       let errors = {};
-      if(!values.name) errors.name = 'Required';
       if (!values.email) {
         errors.email = 'Required';
       } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
@@ -36,9 +34,6 @@ function App() {
           <div className="container__login">
             <form onSubmit={formik.handleSubmit}>
               <h1 className="login__tittle ">Welcome</h1>
-              <div>Name:</div>
-              <input type="text" name="name" id="nameField"onChange={formik.handleChange} value={formik.values.name}/>
-              {formik.errors.name ? <div id="nameError">{formik.errors.name}</div> : null}
               <div>Email:</div>
               <input type="text" name="email" id="emailField" onChange={formik.handleChange} value={formik.values.email}/>
               {formik.errors.email ? <div id="emailError" >{formik.errors.email}</div> : null}        
